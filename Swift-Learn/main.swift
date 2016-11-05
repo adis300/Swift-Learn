489b34c063625cd06d430fa0f83c7c7e80ab3f58
Disi A <adis@live.cn>
2016-11-05 14:57:26 -0400

    Initial setup.

---
+++ Swift-Learn/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-Learn
+//
+//  Created by Disi A on 11/5/16.
+//  Copyright © 2016 Votebin. All rights reserved.
+//
+
+import Foundation
+
+print("Test framework here!")
+
