8cebbbe0a44987897841fe7de48f4f53d0bdbf90
Disi A <adis@live.cn>
2016-11-07 09:55:47 -0500

    Matrix init & subscript.

--- Swift-Learn/Source/Matrix.swift
+++ Swift-Learn/Source/Matrix.swift
@@ -10,7 +10,24 @@ import Accelerate
 
 struct Matrix {
     
+    // Flat matrix implementation
+    var matrix: [Double]
+    let size: (Int, Int)
+    
     init(row: Int, col: Int) {
+        size = (row,col)
+        matrix = [Double](repeating:0, count: row*col)
     }
+    
+    // MARK: Indexed getter setter implementation
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            return matrix[row * size.1 + column]
+        }
+        set(newValue) {
+            return matrix[row * size.1 + column] = newValue
+        }
+    }
+    
 }
 
